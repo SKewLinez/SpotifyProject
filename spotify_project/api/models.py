95d4e36ea5ea02a58519c836184228b815f6619b
SKewLinez <skewlinezofficial@gmail.com>
2021-08-04 21:36:12 +0930

    Resolve the key issue.

--- spotify_project/api/models.py
+++ spotify_project/api/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Party(models.Model):
+    code = models.CharField(max_length=8, default="", unique=True)
+    host = models.CharField(max_length=50, unique=True)
+    received_votes = models.IntegerField(null=False, default=0)
+    guest_can_pause = models.BooleanField(null=False, default=False)
+    created_at = models.DateTimeField(autp_now_add=True)
