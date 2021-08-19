433273dff5a67855b414bbbeb1595409de4c486c
Skyler <37243931+SKewLinez@users.noreply.github.com>
2021-08-20 00:27:49 +0930

    Update spotify_scraper.py
    Please do not expose the API keys.

--- munya/spotify_scraper.py
+++ munya/spotify_scraper.py
@@ -1,8 +1,8 @@
 import spotipy
 from spotipy.oauth2 import SpotifyClientCredentials
 
-sp = spotipy.Spotify(auth_manager=SpotifyClientCredentials(client_id="4b5e303e48ae402b99cb3350155f4961",
-                                                           client_secret="59acc532db204251bfb42452982299e9"))
+sp = spotipy.Spotify(auth_manager=SpotifyClientCredentials(client_id="",
+                                                           client_secret=""))
 
 results = sp.search(q='weezer', limit=50)
 for idx, track in enumerate(results['tracks']['items']):
